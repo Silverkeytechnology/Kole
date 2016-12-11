6282386f4ce9c09d1740340354ecf0e50fd9e6d6
Chaiwa Berian <chaiwa.berian@gmail.com>
2016-12-11 20:06:10 +0200

    Complete defining server.js with db and app connections

--- bin/server.js
+++ bin/server.js
@@ -3,25 +3,36 @@
 //Setting up handles
 let express = require('express');
 let app = express();
-let crossenv = require('cross-env');
+let dotenv = require('dotenv');
+let mongoose = require('mongoose');
 
-let port = 3000;
-
+//load database configurations
 let configFile = '.env';
 if (process.env.NODE_ENV == "test") {
   configFile = '.env.test';
 }
+dotenv.load({ path: configFile });                //this will read the.env or .env.test file, parse the contents, and assign it to process.env
 
-//Setup server to listen for connections
-app.listen(port, function(err, callback) {
+//setup a connection to the server using mongoose
+mongoose.connect(process.env.MONGODB);
+let db = mongoose.connection;
+//listen for connection erros
+db.on('error', console.error.bind(console, 'connection error:', chalk.red('✗')));
+//open connection
+db.on('open', function() {
+  //we are connected
+  console.log("Connected correcly to the database server", chalk.green('✓'));
+});
+
+
+//Setup application server to listen for connections
+let server = app.listen(process.env.PORT, function(err, callback) {
   if (err) {
     console.log(err);
   }
 
-  console.log("App listening at port : " + port);
+  console.log('Kole is listening on port '+ process.env.PORT + '!', chalk.green('✓'));
 });
 
-//Setup routes
-app.get('/api', function(req, res) {
-  res.send("This Home Page for You!!");
-})
+//Expose the express server object so it can be accessed outside this module
+module.exports = server;
