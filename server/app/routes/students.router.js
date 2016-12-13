36f397ba2909b210be29a5ab23c84cb85df05008
Chaiwa Berian <chaiwa.berian@gmail.com>
2016-12-13 21:35:29 +0200

    Comment out create/post route stub to fix  post errror

--- server/app/routes/students.router.js
+++ server/app/routes/students.router.js
@@ -6,13 +6,13 @@ router.route('/')
   //Get All students
   .get(controller.getAllStudents)
   //Add a new student
-  .post(controller.createStudent)
+  //.post(controller.createStudent)
 router.route('/:id')
   //GET a specific student
   .get(controller.findStudentById)
   //Update student record
-  .put(controller.updateStudent)
+  //.put(controller.updateStudent)
   //Delete Student record
-  .delete(controller.deleteStudent)
+  //.delete(controller.deleteStudent)
 
   module.exports = router;
