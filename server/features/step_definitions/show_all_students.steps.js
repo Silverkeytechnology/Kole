0282395271d380c0673f68732b61b040ab4ad9a2
Chaiwa Berian <chaiwa.berian@gmail.com>
2016-12-15 18:11:09 +0200

    Add step definitions for #26

--- server/features/step_definitions/show_all_students.steps.js
+++ server/features/step_definitions/show_all_students.steps.js
@@ -15,17 +15,28 @@ module.exports = function () {
   let self = this;
 
   this.Given(/^there are students in the system$/, function(callback) {
-    //code heree
-    callback(null, 'pending');
+    //code here
+    new Student({'firstName': 'John','lastName': 'Banda','gender': 'Male'}).save();
+    //if need for a promise arises add it here
+    callback();
   });
 
-  this.When(/^the client requests for a list of studnts$/, function(callback) {
-    //code heree
-    callback(null, 'pending');
+  this.When(/^the client requests for a list of students$/, function(callback) {
+    //code here
+    chai.request(server)
+    .get('/api/students')
+    .end((err, res) => {
+      if (err) throw err;
+      self.res = res;
+      callback();
+    });
   });
 
   this.Then(/^the response should be the list of all students in the system$/, function(callback) {
-    //code heree
-    callback(null, 'pending');
+    //code here
+    assert.equal(self.res.status, 200);
+    assert.isArray(self.res.body);
+    //find ways of asserting that it contains the most relevant fields and types
+    callback();
   });
 }
