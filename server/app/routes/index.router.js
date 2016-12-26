76c03822096353442fec5aee80aa735409460fce
Chaiwa Berian <chaiwa.berian@gmail.com>
2016-12-26 11:32:55 +0200

    Remove index.router.js

--- server/app/routes/index.router.js
+++
@@ -1,6 +0,0 @@
-let router = require('express').Router();
-
-//Redirect all requests to home to Index page
-router.route('/').get('/', function(req, res, next) {
-  res.render('view', 'index.html');
-});
